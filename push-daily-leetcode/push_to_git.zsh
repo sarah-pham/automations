6caec35d22c89d56672711b954bfafaaae8f4d22
Sarah Pham <sarahdzung@gmail.com>
2024-09-29 00:57:05 +1000

    Add support for daily problems with class names other than Solution

--- push-daily-leetcode/push_to_git.zsh
+++ push-daily-leetcode/push_to_git.zsh
@@ -3,21 +3,28 @@
 src_file="Solution.java"
 git_path="../git/leetcode/"
 
-if [[ $# -gt 1 || ($# -eq 1 && $1 -ne "incomplete") ]]; then
+if [[ $# -gt 1 ]]; then
     echo "Invalid arguments"
     exit
 fi
-incomplete=$#
 
-javac $src_file "ShellHelper.java"
-methods_pattern=$(java ShellHelper printSolutionMethods)
+if [[ $# -eq 1 ]]; then
+    problem=$(
+        basename -s .java $1 |
+        sed -e 's/\([a-z]\)\([A-Z]\)/\1-\2/g' |
+        tr '[:upper:]' '[:lower:]'
+    )
+else
+    javac $src_file "ShellHelper.java"
+    methods_pattern=$(java ShellHelper printSolutionMethods)
 
-problem=$(
-    grep -oE "$methods_pattern" "$src_file" |   # Search for methods in file
-    head -n 1 |                 # Limit to first
-    sed -e 's/[A-Z]/-&/g' |     # Add hyphen before each uppercase char
-    tr '[:upper:]' '[:lower:]'  # Convert to lowercase
-)
+    problem=$(
+        grep -oE "$methods_pattern" "$src_file" |   # Search for methods in file
+        head -n 1 |                 # Limit to first
+        sed -e 's/[A-Z]/-&/g' |     # Add hyphen before each uppercase char
+        tr '[:upper:]' '[:lower:]'  # Convert to lowercase
+    )
+fi
 
 current_date=$(date -u "+%Y-%m-%d")
 new_dir=($current_date"_"$problem)
@@ -29,10 +36,6 @@ if [ ! -d "$dest_path" ]; then
     git_msg="Add solution to $current_date"
 else
     git_msg="Update solution to $current_date"
-fi
-
-if [[ incomplete -eq 1 ]]; then
-    git_msg="[Incomplete] "$git_msg
 fi
 
 git -C $git_path add $new_dir
